96ab7de68fff1466c02f0f367cd0488f8f5cda42
root <root@kali.home>
2020-04-05 00:24:38 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ function addNavListener(){
 };
 //Popular Page
 axios
-.get(proxy+'https://www.travelpayouts.com/whereami?locale=en&ip=')
+.get('https://www.travelpayouts.com/whereami?locale=en&ip=')
 .then(response=>{
     state.Cheapest.origin = response.data.iata; 
     tickets(response.data.iata);
