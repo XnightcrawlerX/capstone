9dd286e8a00b511ca17d44f95cfb97c10712b277
root <root@kali.home>
2020-04-16 21:47:01 -0500

    modified:   components/views/Home.js 	modified:   components/views/Popular.js 	modified:   styles1-0.css 	deleted:    ipad.css 	deleted:    iphone6-7-8-375x667.css 	deleted:    iphoneX-375x812.css 	deleted:    laptop.css 	deleted:    max1300.css 	deleted:    max400.css 	deleted:    mobile.css 	deleted:    monitor.css 	deleted:    styles.css

--- components/views/Home.js
+++ components/views/Home.js
@@ -2,15 +2,7 @@ export default () => {
     return `
 <section class="main">
     <div class="form-container">
-    <form>
-    <label for="origin">Origin city</label><br>
-    <input type="text" id="origin" name="origin" placeholder="Leave blank for current city"><br>
-    <label for="destination">Destination city</label><br>
-    <input type="text" id="destination" name="destination"><br>
-    <label for="arriving">Returning</label><br>
-    <input type="date" id="arriving" name="arriving"><br>
-    <input type="submit" id="submit" value="Results">
-  </form> 
+      <a href="http://jetradar.com/?marker=269711">Search Flight</a>
     </div>
 </section>
 `
--- components/views/Popular.js
+++ components/views/Popular.js
@@ -9,7 +9,7 @@ ${st.picture.map((result, index)=>{
 
 function formatResult(url, city, price){
     return`
-    <a href="#" alt="${city}:${price}" style="background: url( ${url} ); background-repeat: no-repeat; background-size: cover; background-position: center;">
+    <a href="/" alt="${city}:${price}" style="background: url( ${url} ); background-repeat: no-repeat; background-size: cover; background-position: center;">
         <div class="city-name">${city}</div>
         <div class="price">$${price}</div>
     </a>
